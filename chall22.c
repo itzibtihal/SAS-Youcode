da71f8cb96e8daa3dec137bc45c730672821734e
itzibtihal <ibtihalboukhanchouch@gmail.com>
2023-09-15 14:31:57 +0100

    challenge 22

---
+++ chall22.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int numb, i, Prem = 1;
+
+    printf("Entrez un entier positif : ");
+    scanf("%d", &numb);
+
+    for (i = 2; i <= numb / 2; ++i) {
+        if (numb % i == 0) {
+            Prem = 0;
+            break;
+        }
+    }
+
+    if (Prem)
+        printf("%d est un nombre premier.", numb);
+    else
+        printf("%d n'est pas un nombre premier.", numb);
+
+    return 0;
+}
