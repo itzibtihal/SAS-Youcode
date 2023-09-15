c4949bac6b5059a35a7f3a1faf2c90fad72a2317
itzibtihal <ibtihalboukhanchouch@gmail.com>
2023-09-15 12:23:08 +0100

    challenge 19

---
+++ chall19.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    
+
+     // initialisation de rand 
+      srand (time(NULL)) ;
+
+      // CHOISIR un nombrebautomatiquement 
+        
+        int jour = (rand() % 8 ) ;
+
+        printf(" %d" ,jour);
+       
+     // affiche  un des jours de la semaine
+         
+         switch (jour
+         )
+         {
+         case '1':
+            printf("lundi ");
+            break;
+
+        case '2':
+            printf("mardi");
+            break;
+        case '3':
+            printf(" mercredi");
+            break;
+        case '4':
+            printf("jeudi");
+            break;
+        case '5':
+            printf("vendredi");
+            break;
+        case '6':
+            printf("samedi");
+            break;
+         
+         default:
+             printf("dimenche");
+            break;
+         }
+      
+
+    return 0;
+}
