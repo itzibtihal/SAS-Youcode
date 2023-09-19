c01cd079c0cd4aed6e2c084de03e675801533dbd
itzibtihal <ibtihalboukhanchouch@gmail.com>
2023-09-19 10:42:46 +0100

    functions-challenges

--- functions/chall1.c
+++ functions/chall1.c
@@ -1,22 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <windows.h>
+ 
+ void bonjour(){
+    system("color 0A");
+    printf("YouCode");
+ }
 
-void setConsoleColor(int textColor, int bgColor) {
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleTextAttribute(hConsole, textColor | (bgColor << 4));
-}
-
-void resetConsoleColor() {
-    setConsoleColor(7, 0); // Réinitialiser aux couleurs par défaut (texte blanc sur fond noir)
-}
-
-void bonjour() {
-    setConsoleColor(14, 1); // Jaune sur fond bleu (vous pouvez utiliser d'autres combinaisons)
-    printf("YouCode\n");
-    resetConsoleColor();
-}
-
-int main() {
+int main (){
     bonjour();
-    return 0;
+    return 0 ;
 }
